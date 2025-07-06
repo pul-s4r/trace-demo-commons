b315bcbb6d722518cf3037dc760097df2170e604
Junji Moey <jjmoey@gmail.com>
2025-07-06 21:35:45 +1000

    TRD-9 - feat: new allocate/deallocate events

---
+++ src/main/java/com/jjmoey/warehousedemo/commons/events/order/OrderItemDeallocatedEventDto.java
@@ -0,0 +1,34 @@
+package com.jjmoey.warehousedemo.commons.events.order;
+
+import com.jjmoey.warehousedemo.commons.AbstractEventDto;
+
+public class OrderItemDeallocatedEventDto extends AbstractEventDto {
+    private Integer orderId;
+    private String skuRef;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getSkuRef() {
+        return skuRef;
+    }
+
+    public void setSkuRef(String skuRef) {
+        this.skuRef = skuRef;
+    }
+
+    @Override
+    public Integer getKey() { 
+        return orderId; 
+    }
+
+    @Override
+    public String getType() { 
+        return OrderItemDeallocatedEventDto.class.getName(); 
+    }
+} 
